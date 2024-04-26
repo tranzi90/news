55a3d184bf279add02b7f61d976efac18a2aa842
denis_levchenko <tranzi90@gmail.com>
2024-04-26 21:40:56 +0300

    add db provider

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class NewsDbProvider {
+  late Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (newDb, version) {},
+    );
+  }
+}
