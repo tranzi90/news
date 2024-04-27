909b9128e9a6160e85488deb175e53eeae96615d
denis_levchenko <tranzi90@gmail.com>
2024-04-27 20:50:02 +0300

    set raw sql query

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:news/src/models/item_model.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -13,7 +14,41 @@ class NewsDbProvider {
     db = await openDatabase(
       path,
       version: 1,
-      onCreate: (newDb, version) {},
+      onCreate: (newDb, version) {
+        newDb.execute("""
+          CREATE TABLE Items 
+            (
+              id INTEGER PRIMARY KEY, 
+              type TEXT,
+              by TEXT, 
+              time INTEGER,
+              text TEXT,
+              parent INTEGER,
+              kids BLOB,
+              dead INTEGER,
+              deleted INTEGER,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendants INTEGER
+            )
+        """);
+      },
     );
+  }
+
+  fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
   }
 }
