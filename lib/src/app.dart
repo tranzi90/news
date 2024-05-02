6929a94192892134c9322604c2c238ca02dfc35f
denis_levchenko <tranzi90@gmail.com>
2024-05-02 22:52:31 +0300

    ++scaffold

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/screens/news_list.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NewsList(),
     );
   }
 }
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -5,6 +5,11 @@ class NewsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: Text('News feed here...'),
+    );
   }
 }
